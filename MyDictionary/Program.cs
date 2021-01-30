621b810f5b8614f722c63fbafe55592742974241
Batuhan Tryk <batuhan_tryk@hotmail.co.uk>
2021-01-30 22:25:37 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> City = new Dictionary<string, int>();
+            City.Add("İstanbul", 34);
+            City.Add("Kocaeli", 41);
+            City.Add("Giresun", 28);
+            Console.WriteLine(City.Count);
+        }
+    }
+}
+
+
+        
